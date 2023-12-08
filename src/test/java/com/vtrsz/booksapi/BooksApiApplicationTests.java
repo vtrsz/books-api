1de6e0f7edd3efbbae13d053f35203d781604676
Vitor Souza <vitorsouzaribeiro1@gmail.com>
2023-12-08 15:59:02 -0300

    feat(tests): application properties added

--- src/test/java/com/vtrsz/booksapi/BooksApiApplicationTests.java
+++ src/test/java/com/vtrsz/booksapi/BooksApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.vtrsz.booksapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "file:src/test/resources/application-test.properties")
 class BooksApiApplicationTests {
 
     @Test
